574dd11ece3cfc67c71609d995471cb730035cfc
patan <patan146@student.liu.se>
2016-11-09 15:14:03 +0100

    lab2 stack.{c|h}

---
+++ lab2/stack.c
@@ -0,0 +1,110 @@
+/*
+ * stack.c
+ *
+ *  Created on: 18 Oct 2011
+ *  Copyright 2011 Nicolas Melot
+ *
+ * This file is part of TDDD56.
+ *
+ *     TDDD56 is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     TDDD56 is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with TDDD56. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef DEBUG
+#define NDEBUG
+#endif
+
+#include <assert.h>
+#include <pthread.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+#include "stack.h"
+#include "non_blocking.h"
+
+#if NON_BLOCKING == 0
+#warning Stacks are synchronized through locks
+#else
+#if NON_BLOCKING == 1
+#warning Stacks are synchronized through hardware CAS
+#else
+#warning Stacks are synchronized through lock-based CAS
+#endif
+#endif
+
+void
+stack_check(stack_t *stack)
+{
+// Do not perform any sanity check if performance is bein measured
+#if MEASURE == 0
+	// Use assert() to check if your stack is in a state that makes sens
+	// This test should always pass
+	assert(1 == 1);
+
+	// This test fails if the task is not allocated or if the allocation failed
+	assert(stack != NULL);
+#endif
+}
+
+void
+stack_push(stack_t s, struct stack_node *node)
+{
+#if NON_BLOCKING == 0
+	pthread_mutex_lock(&s->lock);
+	node->next = s->head;
+	s->head = node;
+	pthread_mutex_unlock(&s->lock);
+#elif NON_BLOCKING == 1
+  do {
+		struct stack_node *old = stack->head;
+		node->next = old;
+		struct stack_node *new = node;
+	} while (cas(&stack->head,old,new)!=old)
+#else
+  /*** Optional ***/
+  // Implement a software CAS-based stack
+#endif
+
+  // Debug practice: you can check if this operation results in a stack in a consistent check
+  // It doesn't harm performance as sanity check are disabled at measurement time
+  // This is to be updated as your implementation progresses
+  stack_check((stack_t*)1);
+
+  return 0;
+}
+
+struct stack_node *
+stack_pop(stack_t s)
+{
+	struct stack_node *node;
+#if NON_BLOCKING == 0
+	pthread_mutex_lock(&s->lock);
+	node = s->head;
+	s->head = node->next;
+	pthread_mutex_unlock(&s->lock);
+#elif NON_BLOCKING == 1
+	do {
+		struct stack_node *old = stack->head;
+		node = old;
+		struct stack_node *new = old->head->next;
+	} while (cas(&stack->head,old,new)!=old)
+#else
+  /*** Optional ***/
+  // Implement a software CAS-based stack
+#endif
+
+  return node;
+}
